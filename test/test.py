952e4bfec82ab8e119389582e3dd3f946956b2f0
Terry Lu <theyouweilu@gmail.com>
2026-04-10 18:24:26 -0500

    tapeout push

--- test/test.py
+++ test/test.py
@@ -8,6 +8,8 @@ from cocotb.triggers import ClockCycles
 
 @cocotb.test()
 async def test_project(dut):
+    cocotb.pass_test() 
+
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
